9e47ecd80fd60df7f6be6dc615ff208c41b3bb2f
bmleeBigby <lee.bryanmitchell@gmail.com>
2021-02-15 03:29:23 -0500

    Second commit

---
+++ src/count.php
@@ -0,0 +1,20 @@
+<?php 
+$conn = mysqli_connect("db", "trucorpAdmin", "trucorpAdmin", "Trucorp", 3306);
+
+if($conn -> connect_errno){
+	echo "Failed to connect to MySQL: ".$mysqli -> connect_error;
+	exit();
+}
+
+echo("<h1>Total Pegawai</h1><br>");
+
+$sql = "SELECT COUNT(id) as jumlah from users";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo($row['jumlah']."<br>");
+    }
+}
+
+?>
